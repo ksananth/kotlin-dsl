6e7887e88f16b0ea0d269aeddf80b7f23d2c19ae
Ananth <anan.busyguy007@gmail.com>
2020-01-16 00:22:46 +0100

    extract to method

--- buildSrc/src/main/java/com/dsl/myapplication/AnalyticsCreator.java
+++ buildSrc/src/main/java/com/dsl/myapplication/AnalyticsCreator.java
@@ -7,6 +7,7 @@ import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -20,16 +21,16 @@ import javax.lang.model.element.Modifier;
 
 class AnalyticsCreator {
 
+    private static final String ANALYTICS_JSON = "/Users/anand/Documents/android/MyApplication5/buildSrc/src/main/resources/latest/report_1.0.json";
     private final String CLASS_FILE_NAME = "AnalyticsInstrumentation";
     private final String INTERFACE_FILE_NAME = "AnalyticsApi";
     private final String PATH = "./app/src/main/java/com/dsl/myapplication/";
 
     public static void main(String[] args) throws Exception {
-        getJson("/Users/anand/Documents/android/MyApplication5/buildSrc/src/main/resources/latest/report_1.0.json");
+        getJson(ANALYTICS_JSON);
     }
 
-    void createAnalyticsClass() throws IOException {
-        TypeName hashMap = ClassName.get(HashMap.class);
+    void createAnalyticsClass() throws Exception {
 
         FieldSpec paramA = FieldSpec
                 .builder(AnalyticsClient.class, "analyticsClient", Modifier.PRIVATE)
@@ -39,17 +40,6 @@ class AnalyticsCreator {
                 .builder(String.class, "language", Modifier.PRIVATE)
                 .build();
 
-
-        //Create Method
-        MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder("category");
-        methodBuilder.addModifiers(Modifier.PUBLIC);
-        methodBuilder.returns(void.class);
-        methodBuilder.addStatement("$T<String , String> hashMap = new HashMap<>()",hashMap);
-        methodBuilder.addParameter(String.class, "args"); //dynamic
-        methodBuilder.addStatement("hashMap.put(\"page.category\", \"fb\")"); //dynamic
-        methodBuilder.addStatement("analyticsClient.trackPage(\"details\", hashMap)");
-        MethodSpec methodSpec = methodBuilder.build();
-
         MethodSpec constructor = MethodSpec.constructorBuilder()
                 .addModifiers(Modifier.PUBLIC)
                 .addParameter(AnalyticsClient.class, "analyticsClient")
@@ -58,15 +48,33 @@ class AnalyticsCreator {
                 .addStatement("this.language = language")
                 .build();
 
-
         //Create Class
         TypeSpec.Builder classBuilder = TypeSpec.classBuilder(CLASS_FILE_NAME);
         classBuilder.addSuperinterface(AnalyticsApi.class);
-                classBuilder.addModifiers(Modifier.PUBLIC);
-                classBuilder.addField(paramA);
-                classBuilder.addField(paramB);
-                classBuilder.addMethod(constructor);
-                classBuilder.addMethod(methodSpec);
+        classBuilder.addModifiers(Modifier.PUBLIC);
+        classBuilder.addField(paramA);
+        classBuilder.addField(paramB);
+        classBuilder.addMethod(constructor);
+
+
+
+
+
+        JSONObject analyticsJsonObj = (JSONObject) readJson(ANALYTICS_JSON);
+        System.out.println(analyticsJsonObj);
+        analyticsJsonObj.keySet().forEach(methodName ->
+        {
+            Object keyvalue = analyticsJsonObj.get(methodName);
+            System.out.println("key: " + methodName); //createAnalyticsClass method
+            JSONObject hashMapValues = (JSONObject) keyvalue;
+
+            //Create Method
+            MethodSpec methodSpec = createMethod(""+methodName, hashMapValues);
+            classBuilder.addMethod(methodSpec);
+
+        });
+
+
         TypeSpec classImpl = classBuilder.build();
 
         JavaFile javaFile = JavaFile.builder("com.dsl.myapplication", classImpl)
@@ -76,6 +84,25 @@ class AnalyticsCreator {
         javaFile.writeTo(System.out);
         PrintStream stream = new PrintStream(PATH + CLASS_FILE_NAME + ".java", "UTF-8");
         javaFile.writeTo(stream);
+    }
+
+    @NotNull
+    private MethodSpec createMethod(String methodName, JSONObject hashMapValues) {
+        TypeName hashMap = ClassName.get(HashMap.class);
+        MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(methodName);
+        methodBuilder.addModifiers(Modifier.PUBLIC);
+        methodBuilder.returns(void.class);
+        methodBuilder.addStatement("$T<String , String> hashMap = new HashMap<>()",hashMap);
+        methodBuilder.addParameter(String.class, "args"); //dynamic
+
+        hashMapValues.keySet().forEach(valueStr ->
+        {
+            Object value = hashMapValues.get(valueStr);
+            System.out.println(valueStr + " : " + value); // createAnalyticsClass hashmap
+            methodBuilder.addStatement("hashMap.put(\""+valueStr+"\", \""+value+"\")"); //dynamic
+        });
+        methodBuilder.addStatement("analyticsClient.trackPage(\"details\", hashMap)");
+        return methodBuilder.build();
     }
 
     private static Object readJson(String filename) throws Exception {
