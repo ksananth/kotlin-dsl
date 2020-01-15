1852a699db483d0ce495ac62dc3a35fc0697f873
Ananth <anan.busyguy007@gmail.com>
2020-01-15 23:30:30 +0100

    Rearrange code

--- buildSrc/src/main/java/com/dsl/myapplication/AnalyticsCreator.java
+++ buildSrc/src/main/java/com/dsl/myapplication/AnalyticsCreator.java
@@ -4,10 +4,8 @@ import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
-import com.squareup.javapoet.WildcardTypeName;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,7 +14,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
-import java.util.Map;
 
 import javax.lang.model.element.Modifier;
 
@@ -31,41 +28,8 @@ class AnalyticsCreator {
         getJson("/Users/anand/Documents/android/MyApplication5/buildSrc/src/main/resources/latest/report_1.0.json");
     }
 
-    void createFile() throws IOException {
-        TypeName wildcard = WildcardTypeName.subtypeOf(Object.class);
-
-        TypeName classOfAny = ParameterizedTypeName.get(ClassName.get(Class.class), wildcard);
-
-        TypeName string = ClassName.get(String.class);
-
-        TypeName mapOfStringAndClassOfAny = ParameterizedTypeName.get(ClassName.get(Map.class), string, classOfAny);
-
-        TypeName hashMapOfStringAndClassOfAny = ParameterizedTypeName.get(ClassName.get(HashMap.class), string, classOfAny);
-
-        FieldSpec fieldSpec = FieldSpec.builder(mapOfStringAndClassOfAny, "ID_MAP")
-                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-                .initializer("new $T()", hashMapOfStringAndClassOfAny)
-                .build();
-
-        TypeSpec fieldImpl = TypeSpec.classBuilder(CLASS_FILE_NAME)
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addField(fieldSpec)
-                .build();
-
-        JavaFile javaFile = JavaFile.builder("com.dsl.myapplication", fieldImpl)
-                .build();
-
-        javaFile.writeTo(System.out);
-
-
-        PrintStream stream = new PrintStream("./app/src/main/java/com/dsl/myapplication/" + CLASS_FILE_NAME , "UTF-8");
-        javaFile.writeTo(stream);
-    }
-
     void createAnalyticsClass() throws IOException {
-        TypeName string = ClassName.get(String.class);
         TypeName hashMap = ClassName.get(HashMap.class);
-        TypeName mapOfStringAndClassOfAny = ParameterizedTypeName.get(ClassName.get(Map.class), string, string);
 
         FieldSpec paramA = FieldSpec
                 .builder(AnalyticsClient.class, "analyticsClient", Modifier.PRIVATE)
@@ -113,25 +77,6 @@ class AnalyticsCreator {
         javaFile.writeTo(stream);
     }
 
-
-    static void getJson(String filename) throws Exception {
-        JSONObject jsonObject = (JSONObject) readJson(filename);
-        System.out.println(jsonObject);
-        jsonObject.keySet().forEach(keyStr ->
-        {
-            Object keyvalue = jsonObject.get(keyStr);
-            System.out.println("key: " + keyStr); //createAnalyticsClass method
-            JSONObject valueObj = (JSONObject) keyvalue;
-
-            valueObj.keySet().forEach(valueStr ->
-            {
-                Object value = valueObj.get(valueStr);
-                System.out.println(valueStr + " : " + value); // createAnalyticsClass hashmap
-
-            });
-        });
-    }
-
     private static Object readJson(String filename) throws Exception {
         FileReader reader = new FileReader(filename);
         JSONParser jsonParser = new JSONParser();
@@ -159,5 +104,23 @@ class AnalyticsCreator {
         }
         PrintStream stream = new PrintStream(PATH + INTERFACE_FILE_NAME + ".java", "UTF-8");
         javaFile.writeTo(stream);
+    }
+
+    static void getJson(String filename) throws Exception {
+        JSONObject jsonObject = (JSONObject) readJson(filename);
+        System.out.println(jsonObject);
+        jsonObject.keySet().forEach(keyStr ->
+        {
+            Object keyvalue = jsonObject.get(keyStr);
+            System.out.println("key: " + keyStr); //createAnalyticsClass method
+            JSONObject valueObj = (JSONObject) keyvalue;
+
+            valueObj.keySet().forEach(valueStr ->
+            {
+                Object value = valueObj.get(valueStr);
+                System.out.println(valueStr + " : " + value); // createAnalyticsClass hashmap
+
+            });
+        });
     }
 }
